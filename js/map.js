66d6f130c81c43961f5eaa97cb060357ea401a8a
eheh135 <rlaxodyd201@gmail.com>
2024-08-23 09:07:03 +0900

    HTML checking edit
    웹 접근성 경고, 오류 수정

--- js/map.js
+++ js/map.js
@@ -1,8 +1,3 @@
-Kakao.init('25a6515ca67754804c14e11617b27ab7');
-Kakao.isInitialized();
-
-console.log(Kakao.isInitialized());
-
 var mapContainer = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var mapOption = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(33.4423379727783, 126.571449734542), //지도의 중심좌표.
