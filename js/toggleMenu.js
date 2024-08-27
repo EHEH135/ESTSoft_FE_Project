ab3fa03c0408374b5cb1ef7809c866071f468f1d
eheh135 <rlaxodyd201@gmail.com>
2024-08-27 13:14:58 +0900

    css edit
    일부 css 수정

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -3,7 +3,6 @@ function toggleMenu() {
     const hamburger = document.querySelector('.navbar-togglebtn');
     const isMenuVisible = menu.style.display === 'flex';
     
-    // 메뉴가 열려있으면 닫고, 닫혀있으면 연다
     menu.style.display = isMenuVisible ? 'none' : 'flex';
     hamburger.style.display = isMenuVisible ? 'block' : 'none';
 }
